f84c24eceeb2622af6b884750b05c6b130fc4bc6
adham yehia <adhamyehia194@gmail.com>
2025-01-30 23:29:15 +0200

    Q3 - Given a Queue, implement a function to reverse the elements of a queue using a stack.

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -57,11 +57,62 @@ namespace Assignment
         }
 
         #endregion
+
+        #region Q3
+
+        public static Queue<T> Reverse<T>(Queue<T>? q)
+        {
+
+            
+            if (q is not null || q?.Count > 1)
+            {
+
+                bool QueueFlag = true;
+
+                bool StackFlag = true;
+
+                T? QueueOutput;
+
+                T? StackOutput;
+
+
+                Stack<T> stack = new Stack<T>();
+
+                while ( QueueFlag )
+                {
+
+                    QueueFlag = q.TryDequeue(out QueueOutput);
+
+                    if (QueueFlag && QueueOutput is not null)
+                        stack.Push(QueueOutput);
+
+                }
+
+                while ( StackFlag )
+                { 
+                
+                    StackFlag = stack.TryPop( out StackOutput);
+                      
+                    if (StackFlag && StackOutput is not null)
+                        q.Enqueue(StackOutput);
+
+                }
+
+                return q;
+
+
+            }
+
+            return q;
+
+        }
+
+        #endregion
         static void Main(string[] args)
         {
 
             #region Q1 - Given an array consists of numbers with size N and number of queries, in each query you will be given an integer X, and you shouldprint how many numbers in array that is greater than X.
-            
+
             //int[] numbers = { 11, 5, 3 };
             //int[] queries = { 1, 5, 13 };
 
@@ -74,14 +125,37 @@ namespace Assignment
 
             #region Q2 - Given a number N and an array of N numbers. Determine if it's palindrome or not.
 
-            int N = 5;
-            int[] array = { 1, 2, 3, 2, 1 };
+            //int N = 5;
+            //int[] array = { 1, 2, 3, 2, 1 };
 
-            bool flag = CheckPalindrome (array, N);
+            //bool flag = CheckPalindrome (array, N);
 
-            Console.WriteLine (flag);
+            //Console.WriteLine (flag);
 
             #endregion
+
+            #region Q3 - Given a Queue, implement a function to reverse the elements of a queue using a stack.
+
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+
+            Reverse<int>(queue);
+
+            foreach (int item in queue)
+            {
+                Console.WriteLine(item);
+            }
+
+            //for (int i = 0; i < queue.Count; i++)
+            //{
+            //    Console.WriteLine(queue.Dequeue());
+            //}
+
+            #endregion
+
+
 
         }
     }
